e3a4c7840f08084fc46711bef976a945c68764e3
boring-km <kms0644804@naver.com>
2021-10-31 22:03:43 +0900

    [master] Rectangle의 속성을 이용해 사용하는 방식

--- src/RuntimeTypeCheckIsImpossible.ts
+++ src/RuntimeTypeCheckIsImpossible.ts
@@ -8,10 +8,10 @@ interface Rectangle extends Square {
 
 type Shape = Square | Rectangle;
 
-// function calculateArea(shape: Shape) {
-//   if (shape instanceof Rectangle) { // 인터페이스 형식이 아닌 값으로 인식된다.
-//     return shape.width * shape.height;  // 당연히 Rectangle의 height도 인식 불가
-//   } else {
-//     return shape.width * shape.width;
-//   }
-// }
+function calculateArea(shape: Shape) {
+  if ('height' in shape) { // 차라리 Rectangle의 속성인 height가 있는지 확인
+    return shape.width * shape.height;  // 타입 체커의 보정
+  } else {
+    return shape.width * shape.width;
+  }
+}
